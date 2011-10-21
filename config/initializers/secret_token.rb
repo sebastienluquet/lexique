3bec927f3b4d4f7e7d777177020dbd7ecb27fd3c
unknown <luquet@.(none)>
2011-10-21 13:19:59 +0200

    intial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LexiqueOnRails::Application.config.secret_token = '4e0f9a0bea55b60d6ea3d66983b7d0371441581fbf78bc599cf3124d5915b297de51bf350e42ebe3897de0c3f42bbf51639c4b552a8e4b2d1951ce8e4b207f9f'
